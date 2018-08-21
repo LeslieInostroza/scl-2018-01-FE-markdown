303d890748e2cdb13da68ad8531d47b8a035c3c0
LeslieInostroza <leslie.ino@gmail.com>
2018-08-21 17:10:09 -0300

    se modifica cli.js

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
 
-var ext = path.extname('/leslie/Laboratoria/ClasesFrontEnd/Proyectos/scl-2018-01-FE-markdown/jajaja.txt');
+var ext = path.extname('/leslie/Laboratoria/ClasesFrontEnd/Proyectos/scl-2018-01-FE-markdown/README.md');
 console.log(ext);
