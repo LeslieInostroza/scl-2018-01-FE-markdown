9707414031eb0e0ca84b05dfc3bcb6502c7fb33c
LeslieInostroza <leslie.ino@gmail.com>
2018-08-21 12:52:59 -0300

    probando path

--- cli.js
+++ cli.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
 const path = require('path');
+
+var ext = path.extname('/leslie/Laboratoria/ClasesFrontEnd/Proyectos/scl-2018-01-FE-markdown/jajaja.txt');
+console.log(ext);
