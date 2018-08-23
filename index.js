2c7b830c072e553882cf82a26516a45f18c5db31
LeslieInostroza <leslie.ino@gmail.com>
2018-08-23 09:26:59 -0300

    probando funciones y metodos

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-//const [, , ...args] = process.argv;
+// const mdLinks = require('./lib/md-links');
+let directory = 'TextFiles';
+let dirBuf = Buffer.from(directory);
 
-fs.readdir('', function(err, files) {
+fs.readdir(dirBuf, (err, files) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(files);
+  }  
+  console.log(files);
+  for (let i = 0; i < files.length; i++) {
+    if (path.extname(files[i]) === '.txt') {
+      console.log(files[i]);
+      fs.readFile(files[i], 'utf8', function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      });
+    }
+  }  
+});
+
+
+// const [, , ...args] = process.argv;
+
+/* fs.readdir(`${args}`, function(err, files) {
   if (err) {
     console.log('Error reading files: ', err);
   }
+  console.log(files);
+  
   for (let i = 0; i < files.length; i++) {
     // read its contents.
-    if (path.extname(files[i] === '.md')) {
+    if (path.extname(files[i]) === '.md') {
       console.log(files[i]);
-      fs.readFile(files[i], function(error, data) {
+      fs.readFile(`${files[i]}`, 'utf-8', function(error, data) {
         if (error) {
           console.log('error', error);
         }
@@ -19,7 +47,7 @@ fs.readdir('', function(err, files) {
       });
     }
   }
-});
+});*/
 
 
 /* var ext = path.extname('/leslie/Laboratoria/ClasesFrontEnd/Proyectos/scl-2018-01-FE-markdown/README.md');
