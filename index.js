f4f1081e3ca7d5ae4c2c5c58606550a40d4721cc
LeslieInostroza <leslie.ino@gmail.com>
2018-08-29 09:51:07 -0300

    linea de link

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ if (require.main === module) {
   const [, , ...args] = process.argv;
 
   mdLinks(args[0]).then((link) => {
-    if (link.length === 0) {
+    /* if (link.length === 0) {
       console.error('Error');
-    }
+    }*/
   }).catch((error) => {
     console.error(error);
   });
