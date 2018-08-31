58f919345e7eef6d800aa134811e0e2a68a88de9
LeslieInostroza <leslie.ino@gmail.com>
2018-08-31 14:58:59 -0300

    se agrega colores a laslineas de codigo

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const path = require('path'); 
 const mdLinks = require('./lib/md-links');
 const options = require('./lib/md-links');
+const colors = require('colors');
 const [,, ...userArg] = process.argv; // posicion de ruta escrita por el usuario
-console.log('AQUI' + userArg);
+console.log('Ruta Entregada: ' + userArg);
 const archivo = userArg[0];
 let pathFileAbs = path.resolve(archivo);
 options.validate = userArg[1];
